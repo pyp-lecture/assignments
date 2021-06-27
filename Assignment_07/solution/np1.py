42cf0f137aeb20e873a7b6f544a653522e751ca5
Thomas Smits <thomsmits@gmail.com>
2021-06-27 16:37:07 +0200

    Update of exercises

--- Assignment_07/solution/np1.py
+++
@@ -1,7 +0,0 @@
-# Aus der Vorlesung
-import numpy as np
-
-a = np.array([[1, 2],[3, 4], [5, 6], [7, 8]])
-print(f"Der shape ist: {a.shape}")
-print(f"Das Array ist {a.ndim}-dimensional")
-print(f"Jedes Element braucht {a.itemsize} Bytes an Speicher")
